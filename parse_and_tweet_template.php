7fb78c702067a713d4da0242a383e0c1975821cc
TeoSoft80 <pratosmart@gmail.com>
2014-12-16 02:08:00 +0100

    changed twitter interface
    Added new twitter interface

--- parse_and_tweet_template.php
+++ parse_and_tweet_template.php
@@ -3,8 +3,14 @@
 //Parser and tweeter per XML #pratopioggia
 //by MT
 
-// require codebird
-require_once("codebird-php-develop/src/codebird.php");
+// Include twitteroauth
+require_once('twitteroauth.php');
+
+// Set keys
+$consumerKey = '';
+$consumerSecret = '';
+$accessToken = '';
+$accessTokenSecret = '';
 
 date_default_timezone_set('UTC');
 $today = date("Ymd");                           // data di oggi
@@ -13,7 +19,8 @@ print_r("Oggi è il ".$today);
 $biometeo_ita_xml=simplexml_load_file("http://data.biometeo.it/PRATO/PRATO_ITA.xml") or die("Error: Cannot create object biometeo_ita");
 $biometeo_eng_xml=simplexml_load_file("http://data.biometeo.it/PRATO/PRATO_ENG.xml") or die("Error: Cannot create object biometeo_eng");
 $lamma_xml=simplexml_load_file("http://www.lamma.rete.toscana.it/previ/ita/xml/comuni_web/dati/prato.xml") or die("Error: Cannot create object lamma");
-$sir_xml=simplexml_load_file("http://www.sir.toscana.it/supports/xml/risks/".$today.".xml") or die("Error: Cannot create object sir");
+//$sir_xml=simplexml_load_file("http://www.sir.toscana.it/supports/xml/risks/".$today.".xml") or die("Error: Cannot create object sir");
+$sir_xml=simplexml_load_file("http://www.sir.toscana.it/supports/xml/risks/20141215.xml") or die("Error: Cannot create object sir");
 
 //data selection for @pratopioggia
 
@@ -71,113 +78,110 @@ print_r($sir_str_4_3);
 $sir_str_5_3=("Rischio ghiaccio Comuni di Vernio Cantagallo (B5): " .$sir_xml->rischi->rischio[4]->area[8]->impatto);
 print_r($sir_str_5_3);
 
-// twitter part autenticazione.
-\Codebird\Codebird::setConsumerKey("------------", "------------");
-$cb = \Codebird\Codebird::getInstance();
-$cb->setToken("------------", "--------------");
- 
-$params = array(
-  'status' => $lamma_str
-);
-$reply = $cb->statuses_update($params);
+
+// Create object
+$tweet = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
+
+// Set status message
+$tweetMessage = $lamma_str;
+
+// Check for 140 characters
+if(strlen($tweetMessage) <= 140)
+{
+    // Post the status message
+    $tweet->post('statuses/update', array('status' => $tweetMessage));
+}
 
 sleep(60);
 
-$params = array(
-  'status' => $biometeo_ita_str
-);
-$reply = $cb->statuses_update($params);
+// Set status message
+$tweetMessage = $biometeo_ita_str;
+
+// Check for 140 characters
+if(strlen($tweetMessage) <= 140)
+{
+    // Post the status message
+    $tweet->post('statuses/update', array('status' => $tweetMessage));
+}
 
 sleep(60);
 
-$params = array(
-  'status' => $biometeo_eng_str
-);
-$reply = $cb->statuses_update($params);
+// Set status message
+$tweetMessage = $biometeo_eng_str;
+
+// Check for 140 characters
+if(strlen($tweetMessage) <= 140)
+{
+    // Post the status message
+    $tweet->post('statuses/update', array('status' => $tweetMessage));
+}
 
 sleep(60);
 
 /////
 
-$params = array(
-  'status' => $sir_str_1_1
-);
-$reply = $cb->statuses_update($params);
+
+// Set status message
+$tweetMessage = $sir_str_1_1;
+
+// Check for 140 characters
+if(strlen($tweetMessage) <= 140)
+{
+    // Post the status message
+    $tweet->post('statuses/update', array('status' => $tweetMessage));
+}
 
 sleep(60);
 
 
-$params = array(
-  'status' => $sir_str_2_1
-);
-$reply = $cb->statuses_update($params);
+// Set status message
+$tweetMessage = $sir_str_2_1;
+
+// Check for 140 characters
+if(strlen($tweetMessage) <= 140)
+{
+    // Post the status message
+    $tweet->post('statuses/update', array('status' => $tweetMessage));
+}
 
 sleep(60);
 
 
-$params = array(
-  'status' => $sir_str_3_1
-);
-$reply = $cb->statuses_update($params);
+// Set status message
+$tweetMessage = $sir_str_3_1;
+
+// Check for 140 characters
+if(strlen($tweetMessage) <= 140)
+{
+    // Post the status message
+    $tweet->post('statuses/update', array('status' => $tweetMessage));
+}
 
 sleep(60);
 
 
-$params = array(
-  'status' => $sir_str_4_1
-);
-$reply = $cb->statuses_update($params);
+// Set status message
+$tweetMessage = $sir_str_4_1;
+
+// Check for 140 characters
+if(strlen($tweetMessage) <= 140)
+{
+    // Post the status message
+    $tweet->post('statuses/update', array('status' => $tweetMessage));
+}
 
 sleep(60);
 
 
-$params = array(
-  'status' => $sir_str_5_1
-);
-$reply = $cb->statuses_update($params);
+// Set status message
+$tweetMessage = $sir_str_5_1;
 
-sleep(60);
-
-
-
-////
-
-$params = array(
-  'status' => $sir_str_1_2
-);
-$reply = $cb->statuses_update($params);
-
-sleep(60);
-
-
-$params = array(
-  'status' => $sir_str_2_2
-);
-$reply = $cb->statuses_update($params);
-
-sleep(60);
-
-
-$params = array(
-  'status' => $sir_str_3_2
-);
-$reply = $cb->statuses_update($params);
-
-sleep(60);
-
-
-$params = array(
-  'status' => $sir_str_4_2
-);
-$reply = $cb->statuses_update($params);
-
-sleep(60);
-
-
-$params = array(
-  'status' => $sir_str_5_2
-);
-$reply = $cb->statuses_update($params);
+// Check for 140 characters
+if(strlen($tweetMessage) <= 140)
+{
+    // Post the status message
+    $tweet->post('statuses/update', array('status' => $tweetMessage));
+}
 
 sleep(60);
 
@@ -185,42 +189,137 @@ sleep(60);
 
 ////
 
-$params = array(
-  'status' => $sir_str_1_3
-);
-$reply = $cb->statuses_update($params);
+// Set status message
+$tweetMessage = $sir_str_1_2;
+
+// Check for 140 characters
+if(strlen($tweetMessage) <= 140)
+{
+    // Post the status message
+    $tweet->post('statuses/update', array('status' => $tweetMessage));
+}
 
 sleep(60);
 
 
-$params = array(
-  'status' => $sir_str_2_3
-);
-$reply = $cb->statuses_update($params);
+// Set status message
+$tweetMessage = $sir_str_2_2;
+
+// Check for 140 characters
+if(strlen($tweetMessage) <= 140)
+{
+    // Post the status message
+    $tweet->post('statuses/update', array('status' => $tweetMessage));
+}
 
 sleep(60);
 
 
-$params = array(
-  'status' => $sir_str_3_3
-);
-$reply = $cb->statuses_update($params);
+// Set status message
+$tweetMessage = $sir_str_3_2;
+
+// Check for 140 characters
+if(strlen($tweetMessage) <= 140)
+{
+    // Post the status message
+    $tweet->post('statuses/update', array('status' => $tweetMessage));
+}
 
 sleep(60);
 
 
-$params = array(
-  'status' => $sir_str_4_3
-);
-$reply = $cb->statuses_update($params);
+// Set status message
+$tweetMessage = $sir_str_4_2;
+
+// Check for 140 characters
+if(strlen($tweetMessage) <= 140)
+{
+    // Post the status message
+    $tweet->post('statuses/update', array('status' => $tweetMessage));
+}
 
 sleep(60);
 
 
-$params = array(
-  'status' => $sir_str_5_3
-);
-$reply = $cb->statuses_update($params);
+// Set status message
+$tweetMessage = $sir_str_5_2;
+
+// Check for 140 characters
+if(strlen($tweetMessage) <= 140)
+{
+    // Post the status message
+    $tweet->post('statuses/update', array('status' => $tweetMessage));
+}
+
+sleep(60);
+
+
+
+////
+
+// Set status message
+$tweetMessage = $sir_str_1_3;
+
+// Check for 140 characters
+if(strlen($tweetMessage) <= 140)
+{
+    // Post the status message
+    $tweet->post('statuses/update', array('status' => $tweetMessage));
+}
+
+sleep(60);
+
+
+// Set status message
+$tweetMessage = $sir_str_2_3;
+
+// Check for 140 characters
+if(strlen($tweetMessage) <= 140)
+{
+    // Post the status message
+    $tweet->post('statuses/update', array('status' => $tweetMessage));
+}
+
+sleep(60);
+
+
+// Set status message
+$tweetMessage = $sir_str_3_3;
+
+// Check for 140 characters
+if(strlen($tweetMessage) <= 140)
+{
+    // Post the status message
+    $tweet->post('statuses/update', array('status' => $tweetMessage));
+}
+
+sleep(60);
+
+
+// Set status message
+$tweetMessage = $sir_str_4_3;
+
+// Check for 140 characters
+if(strlen($tweetMessage) <= 140)
+{
+    // Post the status message
+    $tweet->post('statuses/update', array('status' => $tweetMessage));
+}
+
+sleep(60);
+
+
+// Set status message
+$tweetMessage = $sir_str_5_3;
+
+// Check for 140 characters
+if(strlen($tweetMessage) <= 140)
+{
+    // Post the status message
+    $tweet->post('statuses/update', array('status' => $tweetMessage));
+}
+
+sleep(60);
 
 
 //Fonti
