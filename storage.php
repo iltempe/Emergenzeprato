787b12826e41844f14dfee573f01fe6476de6210
IlTempe <teosoft80@gmail.com>
2015-08-27 09:55:01 +0200

    Update storage.php

--- storage.php
+++ storage.php
@@ -2,13 +2,17 @@
 
 <?php
 
-//da far girare con CRON nella folder corrente per salvare i file XML nella cartella data
+//da far girare con CRON nella folder corrente per salvare i file XML nella cartella data. Valutare indicativamente l'orario di aggiornamento delle fonti
 date_default_timezone_set('UTC');
 $today = date("Ymd");   
 
+//si aggiorna verso le 10.00 del mattino
 $biometeo_ITA_file = "http://data.biometeo.it/PRATO/PRATO_ITA.xml";
 $biometeo_ENG_file = "http://data.biometeo.it/PRATO/PRATO_ENG.xml";
+//si aggiorna verso le 8.30 del mattino
 $meteo_file = "http://www.lamma.rete.toscana.it/previ/ita/xml/comuni_web/dati/prato.xml";
+
+//si aggiorna dalle 10 alle 13 del mattino
 $risk_file = "http://www.sir.toscana.it/supports/xml/risks_395/".$today.".xml";
 
 store($biometeo_ITA_file, "data/biometeo_ITA.xml");
