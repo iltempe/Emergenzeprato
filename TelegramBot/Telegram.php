23e9a93204c822b5f9d217efc48587da0bde8ebe
TeoSoft80 <pratosmart@gmail.com>
2015-09-02 08:24:14 +0200

    added web hook version in update.php

--- TelegramBot/Telegram.php
+++ TelegramBot/Telegram.php
@@ -13,117 +13,140 @@ class Telegram {
 	private $data = array();
 	private $updates = array();
 	
-	public function __construct($bot_id) {  
-            $this->bot_id = $bot_id;
-            $this->data = $this->getData();
-	}
-	public function sendMessage(array $content) {
-		$url = 'https://api.telegram.org/bot' . $this->bot_id . '/sendMessage';
-		$this->sendAPIRequest($url, $content);
-
-	}
-
-	public function sendLocation(array $content) {
-		$url = 'https://api.telegram.org/bot' . $this->bot_id . '/sendLocation';
-		$this->sendAPIRequest($url, $content);
-	}
-	
-	public function sendPhoto(array $content) {
-		$url = 'https://api.telegram.org/bot' . $this->bot_id . '/sendPhoto';
-		$this->sendAPIRequest($url, $content);
-	}
-	
-	public function getData() {
-		if ( empty($this->data)) {
-			$rawData = file_get_contents("php://input");
-			return json_decode($rawData,true);
-		}else {
-			return $this->data;
-		}
-	}
-	public function setData(array $data) {
-		$this->data = data;
-		
-	}
-	public function Text() {
-		return $this->data["message"] ["text"];
-	}
-	public function ChatID() {
-		return $this->data["message"]["chat"]["id"];
-	}
-	public function Date() {
-		return $this->data["message"]["date"];
-	}
-	public function FirstName() {
-		return $this->data["message"]["from"]["first_name"];
-	}
-	public function LastName() {
-		return $this->data["message"]["from"]["last_name"];
-	}
-	public function Username() {
-		return $this->data["message"]["from"]["username"];
-	}
-	public function UpdateID() {
-		return $this->data["update_id"];
-	}
-	public function UpdateCount() {
-		return count($this-> updates["result"]);
-
-	}
-	public function messageFromGroup() {
-		if ($this->data["message"]["chat"]["title"] == "") {
-			return false;
-		}
-		return true;
-	}
-	public function buildKeyBoard(array $options, $onetime=true, $resize=true, $selective=true) {
-		$replyMarkup = array(
-			'keyboard' => $options,
-			'one_time_keyboard' => $onetime,
-			'resize_keyboard' => $resize,
-			'selective'		=> $selective
-		);
-		$encodedMarkup = json_encode($replyMarkup,true);
-		return $encodedMarkup;
-	}
-	
-	public function getUpdates($offset = 0, $limit = 100, $timeout = 0, $update = true) {
-		$url = 'https://api.telegram.org/bot' . $this->bot_id . '/getUpdates?offset=' . $offset . '&limit='. $limit .'&timeout=' . $timeout;
-		$reply = $this->getAPIUpdates($url);
-		$this->updates = json_decode($reply,true);
-		if ($update) {
-			$last_element_id = $this->updates["result"][count($this->updates["result"])-1]["update_id"]+1;
-			$url = 'https://api.telegram.org/bot' . $this->bot_id . '/getUpdates?offset=' . $last_element_id . '&limit=1&timeout=' . $timeout;
-			$this->getAPIUpdates($url);
-		}
-		
-		return $this->updates;
+ public function __construct($bot_id) {
+        $this->bot_id = $bot_id;
+        $this->data = $this->getData();
+    }
+    public function getMe() {
+        return $this->endpoint("getMe", array(), false);
+    }
+    public function sendMessage(array $content) {
+        return $this->endpoint("sendMessage", $content);
+    }
+    public function endpoint($api, array $content, $post = true) {
+        $url = 'https://api.telegram.org/bot' . $this->bot_id . '/' . $api;
+        if ($post)
+            return $this->sendAPIRequest($url, $content);
+        else
+            return $this->sendAPIRequest($url, array(), false);
+    }
+    public function sendPhoto(array $content) {
+        return $this->endpoint("sendPhoto", $content);
+    }
+    public function sendAudio(array $content) {
+        return $this->endpoint("sendAudio", $content);
+    }
+    public function sendDocument(array $content) {
+        return $this->endpoint("sendDocument", $content);
+    }
+    public function sendSticker(array $content) {
+        return $this->endpoint("sendSticker", $content);
+    }
+    public function sendVideo(array $content) {
+        return $this->endpoint("sendVideo", $content);
+    }
+    public function sendVoice(array $content) {
+        return $this->endpoint("sendVoice", $content);
+    }
+    public function sendLocation(array $content) {
+        return $this->endpoint("sendLocation", $content);
+    }
+    public function sendChatAction(array $content) {
+        return $this->endpoint("sendChatAction", $content);
+    }
+    public function setWebhook($url) {
+        $content = array('url' => $url);
+        return $this->endpoint("setWebhook", $content);
+    }
+    public function getData() {
+        if (empty($this->data)) {
+            $rawData = file_get_contents("php://input");
+            return json_decode($rawData, true);
+        } else {
+            return $this->data;
+        }
+    }
+    public function setData(array $data) {
+        $this->data = data;
+    }
+    public function Text() {
+        return $this->data["message"] ["text"];
+    }
+    public function ChatID() {
+        return $this->data["message"]["chat"]["id"];
+    }
+    public function Date() {
+        return $this->data["message"]["date"];
+    }
+    public function FirstName() {
+        return $this->data["message"]["from"]["first_name"];
+    }
+    public function LastName() {
+        return $this->data["message"]["from"]["last_name"];
+    }
+    public function Username() {
+        return $this->data["message"]["from"]["username"];
+    }
+    public function Location() {
+        return $this->data["message"]["location"];
+    }
+    public function UpdateID() {
+        return $this->data["update_id"];
+    }
+    public function UpdateCount() {
+        return count($this->updates["result"]);
+    }
+    public function messageFromGroup() {
+        if ($this->data["message"]["chat"]["title"] == "") {
+            return false;
+        }
+        return true;
+    }
+    public function buildKeyBoard(array $options, $onetime = true, $resize = true, $selective = true) {
+        $replyMarkup = array(
+            'keyboard' => $options,
+            'one_time_keyboard' => $onetime,
+            'resize_keyboard' => $resize,
+            'selective' => $selective
+        );
+        $encodedMarkup = json_encode($replyMarkup, true);
+        return $encodedMarkup;
+    }
+    public function getUpdates($offset = 0, $limit = 100, $timeout = 0, $update = true) {
+        $content = array('offset' => $offset, 'limit' => $limit, 'timeout' => $timeot);
+        $reply = $this->endpoint("getUpdates", $content);
+        $this->updates = json_decode($reply, true);
+        if ($update) {
+            $last_element_id = $this->updates["result"][count($this->updates["result"]) - 1]["update_id"] + 1;
+            $content = array('offset' => $last_element_id, 'limit' => "1", 'timeout' => $timeot);
+            $this->endpoint("getUpdates", $content);
+        }
+        return $this->updates;
     }
     public function serveUpdate($update) {
-		$this->data = $this->updates["result"][$update];
-		
-	}
-	private function sendAPIRequest($url, array $content) {
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_HEADER, false);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		//curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($content));
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $content);
-		curl_exec($ch);
-		curl_close($ch);
-	}
-	private function getAPIUpdates($url) {
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_HEADER, false);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		$result = curl_exec($ch);
-		curl_close($ch);
-		return $result;
-	}
-        
+        $this->data = $this->updates["result"][$update];
+    }
+    private function sendAPIRequest($url, array $content, $post = true) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        if ($post) {
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $content);
+        }
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
+    }
 }
-
+// Helper for Uploading file using CURL
+if (!function_exists('curl_file_create')) {
+    function curl_file_create($filename, $mimetype = '', $postname = '') {
+        return "@$filename;filename="
+                . ($postname ? : basename($filename))
+                . ($mimetype ? ";type=$mimetype" : '');
+    }
+}
 ?>
--- TelegramBot/update.php
+++ TelegramBot/update.php
@@ -6,7 +6,7 @@
 
  */
 
-include(dirname(__FILE__).'/../settings.php');
+include('settings.php');
 include("Telegram.php");
  
 // Set the bot TOKEN in setting.php
@@ -24,25 +24,123 @@ $telegram = new Telegram($bot_id);
 $text = $telegram->Text();
 $chat_id = $telegram->ChatID();
 
-// Check if the text is a command
-if ($text == "/test") {
-	if ($telegram->messageFromGroup()) {
-		$reply = "Chat Group";
-	} else {
-		$reply = "Private Chat";
+if ($text == "/start") {
+    	create_keyboard($telegram,$chat_id);
+		$log=$today. ";new chat started;" .$chat_id. "\n";
+		file_put_contents($logfile, $log, FILE_APPEND | LOCK_EX);
 	}
-        // Create option for the custom keyboard. Array of array string
-        $option = array( array("A", "B"), array("C", "D") );
-        // Get the keyboard
-	$keyb = $telegram->buildKeyBoard($option);
-	$content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => $reply);
-	$telegram->sendMessage($content);
+	//richiedi previsioni meteo di oggi
+	elseif ($text == "/meteo" || $text == "meteo") {
+		$reply = "Previsioni Meteo per oggi " .$data->lamma_text("oggi").$data->biometeo_text("oggi");
+		$content = array('chat_id' => $chat_id, 'text' => $reply);
+		$telegram->sendMessage($content);
+		$log=$today. ";meteo sent;" .$chat_id. "\n";
+		file_put_contents($logfile, $log, FILE_APPEND | LOCK_EX);
+		//aggiorna tastiera
+	    create_keyboard($telegram,$chat_id);	
+		}
+	//richiede previsioni meteo di domani
+	elseif ($text == "/previsioni" || $text == "previsioni") {
+		$reply = "Previsioni Meteo per domani " .$data->lamma_text("domani").$data->biometeo_text("domani");
+		$content = array('chat_id' => $chat_id, 'text' => $reply);
+		$telegram->sendMessage($content);
+		$log=$today. ";previsioni sent;" .$chat_id. "\n";
+		file_put_contents($logfile, $log, FILE_APPEND | LOCK_EX);
+		//aggiorna tastiera
+	   create_keyboard($telegram,$chat_id);
+	}
+	//richiede rischi di oggi a Prato
+	elseif ($text == "/rischi" || $text == "rischi") {
+		$reply = "Rischi di oggi:\r\n".$data->risk_text("oggi","B").$data->risk_text("oggi","R1");
+		$content = array('chat_id' => $chat_id, 'text' => $reply);
+		$telegram->sendMessage($content);
+		$log=$today. ";rischi sent;" .$chat_id. "\n";
+		file_put_contents($logfile, $log, FILE_APPEND | LOCK_EX);
+		//aggiorna tastiera
+	  	create_keyboard($telegram,$chat_id);
+	}
+	//crediti
+	elseif ($text == "/crediti" || $text == "crediti") {
+		 $reply = "Applicazione sviluppata da Matteo Tempestini, dettagli e fonti dei dati presenti su : http://pratosmart.teo-soft.com/emergenzeprato/";
+		 $content = array('chat_id' => $chat_id, 'text' => $reply);
+		 $telegram->sendMessage($content);
+		 $log=$today. ";crediti sent;" .$chat_id. "\n";
+		 file_put_contents($logfile, $log, FILE_APPEND | LOCK_EX);
+		 //aggiorna tastiera
+		 create_keyboard($telegram,$chat_id);
+	}
+	//richiede la temperatura
+	elseif ($text == "/temperatura" || $text == "temperatura") {
+	 
+	 	 create_keyboard_temp($telegram,$chat_id);	
+	}
+	elseif ($text =="Prato" || $text == "/temp-prato")
+	{
+		 $reply = "Temperatura misurata in zona Prato Est : " .$data->get_temperature("prato est");
+		 $content = array('chat_id' => $chat_id, 'text' => $reply);
+		 $telegram->sendMessage($content);
+		 $log=$today. ";temperatura Prato sent;" .$chat_id. "\n";
+		 file_put_contents($logfile, $log, FILE_APPEND | LOCK_EX);
+		 //aggiorna tastiera
+		 create_keyboard($telegram,$chat_id);
+	}
+	elseif ($text =="Vaiano/Sofignano" || $text == "/temp-vaianosofignano")
+	{
+		 $reply = "Temperatura misurata in zona Vaiano/Sofignano : " .$data->get_temperature("vaiano sofignano");
+		 $content = array('chat_id' => $chat_id, 'text' => $reply);
+		 $telegram->sendMessage($content);
+		 $log=$today. ";temperatura Vaiano/Sofignano sent;" .$chat_id. "\n";
+		 file_put_contents($logfile, $log, FILE_APPEND | LOCK_EX);
+		 //aggiorna tastiera
+		 create_keyboard($telegram,$chat_id);
+	}
+	elseif ($text =="Vaiano/Schignano" || $text == "/temp-vaianoschignano")
+	{
+		 $reply = "Temperatura misurata in zona Vaiano/Schignano : " .$data->get_temperature("vaiano schignano");
+		 $content = array('chat_id' => $chat_id, 'text' => $reply);
+		 $telegram->sendMessage($content);
+		 $log=$today. ";temperatura Vaiano/Schignano sent;" .$chat_id. "\n";
+		 file_put_contents($logfile, $log, FILE_APPEND | LOCK_EX);
+		 //aggiorna tastiera
+		 create_keyboard($telegram,$chat_id);
+	}
+	elseif ($text =="Montepiano/Vernio" || $text == "/temp-montepianovernio")
+	{
+		 $reply = "Temperatura misurata in zona Montepiano/Vernio : " .$data->get_temperature("montepiano vernio");
+		 $content = array('chat_id' => $chat_id, 'text' => $reply);
+		 $telegram->sendMessage($content);
+		 $log=$today. ";temperatura Montepiano/Vernio sent;" .$chat_id. "\n";
+		 file_put_contents($logfile, $log, FILE_APPEND | LOCK_EX);
+		 //aggiorna tastiera
+		 create_keyboard($telegram,$chat_id);
+	}
+	//comando errato
+	else{
+		 $reply = "Hai selezionato un comando non previsto. Per informazioni visita : http://pratosmart.teo-soft.com/emergenzeprato/";
+		 $content = array('chat_id' => $chat_id, 'text' => $reply);
+		 $telegram->sendMessage($content);
+		 $log=$today. ";wrong command sent;" .$chat_id. "\n";
+		 file_put_contents($logfile, $log, FILE_APPEND | LOCK_EX);	
+	 }
 }
-if ($text == "/git") {
-    $reply = "Check me on GitHub: https://github.com/Eleirbag89/TelegramBotPHP";
-    // Build the reply array
-    $content = array('chat_id' => $chat_id, 'text' => $reply);
-    $telegram->sendMessage($content);
+
+// Crea la tastiera
+function create_keyboard($telegram, $chat_id)
+{
+		$option = array(["meteo","previsioni"],["rischi", "temperatura"],["crediti"]);
+    	$keyb = $telegram->buildKeyBoard($option, $onetime=false);
+		$content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "Seleziona un'opzione per essere aggiornato");
+		$telegram->sendMessage($content);
+}
+
+//crea la tastiera per scegliere la zona temperatura
+function create_keyboard_temp($telegram, $chat_id)
+{
+		$option = array(["Prato","Vaiano/Sofignano"],["Vaiano/Schignano", "Montepiano/Vernio"]);
+    	$keyb = $telegram->buildKeyBoard($option, $onetime=false);
+    	$content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "Seleziona la zona di cui vuoi sapere la temperatura");
+		$telegram->sendMessage($content);
+
 }
 
 ?>
