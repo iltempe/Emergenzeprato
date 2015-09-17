4f1201d77598aa0c8a086806ace68f1330ac4d15
iltempe <pratosmart@gmail.com>
2015-09-17 23:00:48 +0200

    fixing

--- scrape.php
+++ scrape.php
@@ -2,6 +2,6 @@
     $page = file_get_contents('http://www.protezionecivile.comune.prato.it/emergenze/');
     $doc = new DOMDocument();
     $doc->loadHTML($page);
-    $node = $doc->getElementById('div.contenitore');
+    $node = $doc->query('//*[@id="main"]/div[2]');
     echo $doc->saveHtml($node), PHP_EOL;
 ?>
