5bba2ae0fdc3935f3433b2e805dd6c4de8c6470c
iltempe <pratosmart@gmail.com>
2015-10-06 07:39:09 +0200

    new file
    start to coding a new manager for alert sending (in main project)

--- alert_main.php
+++ alert_main.php
@@ -3,7 +3,7 @@
 <?php
 
 //main file to define and send alert
-//to be managed as CRON JOB
+//to be managed as CRON JOB to send alert 
 
 require_once __DIR__.'/./HTMLscraper/src/ScraperInterface.php';
 require_once __DIR__.'/./HTMLscraper/src/Scraper.php';
@@ -22,15 +22,59 @@ $titolo = $object->execute('#head1 h1', $html);
 //seleziono il contenuto
 $descrizione = $object->execute('#main div div', $html);
 
+//rimuovo testo inutile
+$descrizione=array_delete($descrizione[1],'Numero verde emergenze 800 30 15 30 
+    
+      
+        Gallerie  fotografiche
+      
+      
+        Video delle emergenze
+      
+      
+        Meteo a Prato  e dintorni
+        
+      
+        Comportamenti in caso di...');
+
+$descrizione=implode("','",$descrizione);
+
+$current=$titolo[0]. "\n". $descrizione;
+
 //stampo il file (basta la prima volta)
 if(!file_exists($file))
 {
-	$current=$titolo[0]. "\n". $descrizione[1][1];
 	file_put_contents($file, $current);
 }
 else{
+	
 	//se il file esiste già fare il confronto
+	$old=file_get_contents($file);
+	if($old!=$current)
+	{
+	
+		send_alert();
+		file_put_contents($file, $current);
+	
+	}
+	else
+	{
+		//se non ci sono aggiornamenti non fare nulla
+	
+	}
+}
 
+
+function send_alert()
+{
+
+	//inserire cosa fare in caso di invio allerta
+	print_r("invio allerta");
+
+}
+
+function array_delete($array, $element) {
+    return array_diff($array, [$element]);
 }
 			
 ?>
