48d56d565510634a913e62568d8d9a35b7b95989
Matteo <pratosmart@gmail.com>
2015-10-29 10:36:22 +0100

    generic links

--- Ubuntu/notifiche_2.sh
+++ Ubuntu/notifiche_2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #notifica rischio di oggi da emergenzeprato
 #argomenti da passare: tipo rischio e zona
-VAR=$(curl -s -G -L 'http://teo-soft.com/emergency/data/risk.xml' | xpath -e '//documento/rischi[@name="oggi"]/rischio[@name="'$1'"]/area[@name="'$2'"]/impatto/text()')
+#put correct link in this command
+VAR=$(curl -s -G -L 'http://./././risk.xml' | xpath -e '//documento/rischi[@name="oggi"]/rischio[@name="'$1'"]/area[@name="'$2'"]/impatto/text()')
 notify-send -u critical -t 10000 -i /home/matteo/meteo.ico 'Rischio '$1'' $VAR
--- Ubuntu/notifiche_3.sh
+++ Ubuntu/notifiche_3.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #notifica rischio di oggi da emergenzeprato
 #parametri giorno e ora
-VAR=$(curl -s -G -L 'http://teo-soft.com/emergency/data/meteo.xml' | xpath -e '//dati/previsione[@idday="'$1'" and @ora="'$2'"]/simbolo/@descr' | sed "s/.* descr=\"\(.*\)\".*/\1/")
+#put correct link in this command
+VAR=$(curl -s -G -L 'http://./././meteo.xml' | xpath -e '//dati/previsione[@idday="'$1'" and @ora="'$2'"]/simbolo/@descr' | sed "s/.* descr=\"\(.*\)\".*/\1/")
 VAR1=$(echo $VAR)
 notify-send -u critical -t 5000 -i /home/matteo/meteo.ico 'Previsioni Meteo' $VAR1
